89285addbf253dbd32289494c367a98b40a06f4a
Naugtur <naugtur@gmail.com>
2016-01-16 22:43:31 +0100

    abstracted it all out and lived happily ever after

--- actions/ACTION_TYPES.js
+++ actions/ACTION_TYPES.js
@@ -1,6 +1,3 @@
 export default {
     NAVIGATION: "@@navigation",
-    REQUEST_ERROR: "@@request_error",
-    REQUEST_START: "@@request_start",
-    REQUEST_END: "@@request_end"
 }
--- components/App.jsx
+++ components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {connect} from "react-redux";
-import { fetchData } from "../actions"
+import { actions as fetchActions } from "../requests"
 import AList from "./AList.jsx";
 import AItem from "./AItem.jsx";
 
@@ -8,30 +8,21 @@ import AItem from "./AItem.jsx";
 function App(props) {
     switch (props.location.name) {
     case "root" :
-        asyncCall(props.dispatch,fetchData,
-            "/api/list",
-            "aList"
-        )
+        asyncCall(props.dispatch,fetchActions.aList)
         return <AList/>
     case "aItem" :
-        asyncCall(props.dispatch,fetchData,
-            `/api/item/${props.location.options.item}`,
-            "aItems",
-            (data) => ({ [props.location.options.item]: data })
-        )
+        asyncCall(props.dispatch,fetchActions.aItems, props.location.options.item)
         return <AItem itemName={props.location.options.item}/>
     default :
         return <div>Not implemented</div>
     }
 }
 
-
 function asyncCall(dispatch, action, ...args){
     setTimeout(function(){
         dispatch(action(...args))
     },0)
 }
-
 
 export default connect(state => ({
     location: state.navigation.location || {}
