eeb66b1d6974eb9e0705cbc21ae6bd84c97519ae
Naugtur <naugtur@gmail.com>
2016-01-16 22:43:31 +0100

    abstracted it all out and lived happily ever after

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { reducers } from "../requests"
 
 import T from "../actions/ACTION_TYPES"
 import {handleActions} from "redux-actions"
@@ -8,41 +9,7 @@ const navigationReducer = handleActions({
 }, { location: null });
 
 
-
-function createReducerFunction(stateKey, reducers) {
-    return function (state, action) {
-        if (reducers[action.type] && stateKey === action.stateKey) {
-            return reducers[action.type](state, action)
-        } else {
-            return (state || {
-                isFetching: false,
-                data: {}
-            })
-        }
-    }
-
-}
-
-const requestReducerSetup = {
-    [T.REQUEST_START]: (state) => {
-        return Object.assign({}, state, {
-            isFetching: true
-        })
-    },
-    [T.REQUEST_END]: (state, {data}) => ({
-            isFetching: false,
-            data: Object.assign({}, state.data, data)
-    }),
-    [T.REQUEST_ERROR]: (state, {error}) => {
-        return Object.assign({}, state, {
-            isFetching: false,
-            error
-        })
-    }
-}
-
 export default combineReducers({
     navigation: navigationReducer,
-    aList: createReducerFunction("aList", requestReducerSetup),
-    aItems: createReducerFunction("aItems", requestReducerSetup)
+    ...reducers
 });
